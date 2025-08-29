d83938668a966a7e6c35f379c83daa9edfa6e941
wiopty <katerynchykanastasiia@gmail.com>
2025-08-29 13:31:52 +0200

    Midi file creation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from modules.colorfind import get_top_colors
 from modules.note_mapping import convert_colors_to_notes
-from music21 import *
-all_blocks_colors = get_top_colors("assets/mona_lisa.jpg")
+from modules.melody_create import create_melody
+
+all_blocks_colors = get_top_colors("assets/colorful_shapes.jpg")
 notes = convert_colors_to_notes(all_blocks_colors,"config/hue_to_note.json" )
-print(notes)
+saved_file, melody_part = create_melody(notes, "melody1.mid")
